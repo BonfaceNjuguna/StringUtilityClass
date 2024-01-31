942f60db583294409101ee4d72f7ca87ce35e3f5
Bonface Njuguna <bonfacenjuguna13@gmail.com>
2024-01-31 15:11:20 +0300

    implement member functions for string class

---
+++ String.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "String.h"
+
+//default constructor
+String::String(){}
+
+//constructor
+String::String(const char* _str){}
+
+//copy constructor
+String::String(const String& _other){}
+
+//destructor
+String::~String(){}
