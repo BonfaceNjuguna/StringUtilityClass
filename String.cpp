ad672374fdc0df173705102f360fe4421cbe58f8
Bonface Njuguna <bonfacenjuguna13@gmail.com>
2024-01-31 15:30:27 +0300

    implemented blank functions

--- String.cpp
+++ String.cpp
@@ -4,7 +4,7 @@
 //default constructor
 String::String(){}
 
-//constructor
+//constructor C-style
 String::String(const char* _str){}
 
 //copy constructor
@@ -12,3 +12,105 @@ String::String(const String& _other){}
 
 //destructor
 String::~String(){}
+
+//length of the string
+size_t String::Length() const
+{
+	
+}
+
+//character at specified index
+char& String::CharacterAt(size_t _index)
+{
+
+}
+
+const char& String::CharacterAt(size_t _index) const
+{
+
+}
+
+bool String::EqualTo(const String& _other) const 
+{
+
+}
+
+//append
+String& String::Append(const String& _str)
+{
+
+}
+
+//prepend
+String& String::Prepend(const String& _str)
+{
+
+}
+
+//C-style
+const char* String::CStr() const
+{
+
+}
+
+//lowercase
+String& String::ToLower()
+{
+
+}
+
+//uppercase
+String& String::ToUpper()
+{
+
+}
+
+size_t String::Find(const String& _str)
+{
+
+}
+
+size_t String::Find(size_t _startIndex, const String& _replace)
+{
+
+}
+
+String& String::Replace(const String& _find, const String& _replace)
+{
+
+}
+
+String& String::ReadFromConsole()
+{
+
+}
+
+String& String::WriteToConsole()
+{
+
+}
+
+bool String::operator==(const String& _other)
+{
+
+}
+
+bool String::operator!=(const String& _other)
+{
+	
+}
+
+String& String::operator=(const String& _str)
+{
+
+}
+
+char& String::operator[](size_t _index)
+{
+
+}
+
+const char& String::operator[](size_t _index) const
+{
+
+}
