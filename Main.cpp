a13e8b43247f534572a1226bfc1646cf7717a048
Bonface Njuguna <bonfacenjuguna13@gmail.com>
2024-02-06 12:23:05 +0300

    added const character at method to differenciate btw the non const

--- Main.cpp
+++ Main.cpp
@@ -3,21 +3,24 @@
 
 int main() {
     String helloStr("Hello, ");
-    String worldStr("Word");
+    String worldStr("World");
 
     String combineString = helloStr + worldStr;
 
     std::cout << "The combined string is: " << combineString.CStr() << std::endl;
     std::cout << "The length of the combined string is: " << combineString.Length() << std::endl;
     std::cout << "The character at index 5 is: " << combineString.CharacterAt(5) << std::endl;
+    combineString.CharacterAt(5) = 'd';
+    const char& ch = combineString.CharacterAt(5);
+    std::cout << "The character at index 5 is: " << ch << std::endl;
 
     if (combineString.EqualTo("Hello, World"))
     {
-        std::cout << "The combined string is equal to 'Hello, World'\n";
+        std::cout << "The combined string is equal to 'Hello, World'" << std::endl;
     }
     else
     {
-        std::cout << "The combined string is not equal to 'Hello, World'\n";
+        std::cout << "The combined string is not equal to 'Hello, World'" << std::endl;
     }
 
     std::cout << "The upper case of the combined string is: " << combineString.ToUpper().CStr() << std::endl;
