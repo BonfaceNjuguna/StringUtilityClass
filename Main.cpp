96fe3d770fce169d0a47439ec442a1dd5ea649c5
Bonface Njuguna <bonfacenjuguna13@gmail.com>
2024-04-09 15:58:57 +0300

    tested the remaining methods

--- Main.cpp
+++ Main.cpp
@@ -26,6 +26,7 @@ int main() {
     else
         std::cout << "The combined string is not equal to 'Hello, World'" << std::endl;
 
+
     //convert to upper and lower case
     std::cout << "The upper case of the combined string is: " << combineString.ToUpper().CStr() << std::endl;
     std::cout << "The lower case of the combined string is: " << combineString.ToLower().CStr() << std::endl;
@@ -36,9 +37,19 @@ int main() {
     //append
     std::cout << "Appending ' Bonface' is: " << helloStr.Append(" Bonface").CStr() << std::endl;
 
+    //find
     size_t foundIndex = findAndReplaceString.Find("inputStr");
     if (foundIndex != SIZE_MAX) {
         std::cout << "Substring found at index: " << foundIndex << std::endl;
+    }
+    else {
+        std::cout << "Substring not found." << std::endl;
+    }
+
+    //find from index
+    size_t findIndex = findAndReplaceString.Find(10, "string");
+    if (findIndex != SIZE_MAX) {
+        std::cout << "Substring found at index: " << findIndex << std::endl;
     }
     else {
         std::cout << "Substring not found." << std::endl;
@@ -52,6 +63,50 @@ int main() {
     //replace
     turkanaStr.Replace("Turkana", "Kenya");
     std::cout << "The string after replacing 'Turkana' with 'Kenya' is: " << turkanaStr.CStr() << std::endl;
+
+    //copy
+    String copiedString = helloStr;
+    std::cout << "Copied string is: " << copiedString.CStr() << std::endl;
+
+    //operator overload equality
+    if (helloStr == worldStr)
+        std::cout << "The strings are equal." << std::endl;
+    else
+        std::cout << "The strings are not equal." << std::endl;
+
+    //operator overload assignment
+    String assignedString;
+    assignedString = worldStr;
+    std::cout << "Assigned string is: " << assignedString.CStr() << std::endl;
+
+    //operator overload subscript
+    std::cout << "Character at index 6 of helloStr is: " << helloStr[6] << std::endl;
+
+    //operator overload inequality
+    if (helloStr != worldStr)
+        std::cout << "The strings are not equal." << std::endl;
+    else
+        std::cout << "The strings are equal." << std::endl;
+
+    //operator overload less than
+    if (helloStr < worldStr)
+        std::cout << "helloStr is less than worldStr." << std::endl;
+    else
+        std::cout << "helloStr is not less than worldStr." << std::endl;
+
+    //operator overload addition
+    String addedString = helloStr + " Kabiru";
+    std::cout << "Added string is: " << addedString.CStr() << std::endl;
+
+    //operator overload addition assignment
+    helloStr += " Kabiru";
+    std::cout << "helloStr after addition assignment: " << helloStr.CStr() << std::endl;
+
+    //read from console
+    String inputString;
+    std::cout << "Enter a string: ";
+    inputString.ReadFromConsole();
+    std::cout << "You entered: " << inputString.CStr() << std::endl;
 
     return 0;
 }
