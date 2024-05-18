5aedadb68c0049b65f203cec95b4c8acf4a289da
Bonface Njuguna <bonfacenjuguna13@gmail.com>
2024-05-18 09:26:04 +0300

    added project for text adventure game and formatted string.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,126 +1,126 @@
-#include "String.h"
-#include <iostream>
-#include <fstream>
-#include <ctime>
-#include <iomanip>
-#include <string>
-
-//log test results to a file
-static void logTestResult(const std::string& testName, bool passed, std::ofstream& logFile, int& successfulTests) {
-    logFile << testName << " " << (passed ? "Successful" : "Failed") << std::endl;
-    if (passed) successfulTests++;
-}
-
-int main() {
-    std::ofstream logFile("log.txt", std::ios::app);
-
-    std::time_t t = std::time(nullptr);
-    std::tm tm;
-    localtime_s(&tm, &t);
-    char buffer[26];
-    asctime_s(buffer, sizeof buffer, &tm);
-
-    logFile << "\nDate: " << std::put_time(&tm, "%d/%m/%Y") << " Time: " << std::put_time(&tm, "%H:%M:%S") << std::endl;
-
-    String helloStr("Hello, ");
-    String worldStr("World");
-    String turkanaStr("Hello Turkana");
-    String findAndReplaceString("This is a test string to be used for search and replace");
-
-    int totalTests = 0;
-    int successfulTests = 0;
-
-    // Test concatenation
-    totalTests++;
-    String combineString = helloStr + worldStr;
-    logTestResult("Test " + std::to_string(totalTests) + " Concatenation", combineString.EqualTo("Hello, World"), logFile, successfulTests);
-
-    // Test length of the combined string
-    totalTests++;
-    logTestResult("Test " + std::to_string(totalTests) + " Length", combineString.Length() == 12, logFile, successfulTests);
-
-    // Test character at index
-    totalTests++;
-    logTestResult("Test " + std::to_string(totalTests) + " CharacterAt", combineString.CharacterAt(0) == 'H', logFile, successfulTests);
-
-    // Test string equality
-    totalTests++;
-    logTestResult("Test " + std::to_string(totalTests) + " EqualTo", combineString.EqualTo("Hello, World"), logFile, successfulTests);
-
-    // Test upper and lower case
-    totalTests++;
-    logTestResult("Test " + std::to_string(totalTests) + " ToUpper", combineString.ToUpper().EqualTo("HELLO, WORLD"), logFile, successfulTests);
-    totalTests++;
-    logTestResult("Test " + std::to_string(totalTests) + " ToLower", combineString.ToLower().EqualTo("hello, world"), logFile, successfulTests);
-
-    // Test prepend
-    totalTests++;
-    logTestResult("Test " + std::to_string(totalTests) + " Prepend", helloStr.Prepend("Mr. ").EqualTo("Mr. Hello, "), logFile, successfulTests);
-
-    // Test append
-    totalTests++;
-    logTestResult("Test " + std::to_string(totalTests) + " Append", helloStr.Append(" Bonface").EqualTo("Mr. Hello,  Bonface"), logFile, successfulTests);
-
-    // Test find
-    totalTests++;
-    size_t foundIndex = findAndReplaceString.Find("search");
-    logTestResult("Test " + std::to_string(totalTests) + " Find", foundIndex != SIZE_MAX, logFile, successfulTests);
-
-    // Test find from index
-    totalTests++;
-    size_t findIndex = findAndReplaceString.Find(10, "string");
-    logTestResult("Test " + std::to_string(totalTests) + " FindFromIndex", findIndex != SIZE_MAX, logFile, successfulTests);
-
-    // Test replace
-    totalTests++;
-    turkanaStr.Replace("Hello", "Hi");
-    logTestResult("Test " + std::to_string(totalTests) + " Replace", turkanaStr.EqualTo("Hi Turkana"), logFile, successfulTests);
-
-    // Test copy
-    totalTests++;
-    String copiedString = helloStr;
-    logTestResult("Test " + std::to_string(totalTests) + " Copy", copiedString.EqualTo(helloStr), logFile, successfulTests);
-
-    // Test operator overload equality
-    totalTests++;
-    logTestResult("Test " + std::to_string(totalTests) + " Operator==", helloStr == helloStr, logFile, successfulTests);
-
-    // Test operator overload assignment
-    totalTests++;
-    String smallString("This is smaller");
-    String largeString("This is a larger string to test assignment");
-    smallString = largeString;
-    logTestResult("Test " + std::to_string(totalTests) + " Operator=", smallString.Length() == largeString.Length(), logFile, successfulTests);
-
-    // Test operator overload subscript
-    totalTests++;
-    logTestResult("Test " + std::to_string(totalTests) + " Operator[]", helloStr[0] == 'M', logFile, successfulTests);
-
-    // Test operator overload inequality
-    totalTests++;
-    logTestResult("Test " + std::to_string(totalTests) + " Operator!=", helloStr != worldStr, logFile, successfulTests);
-
-    // Test operator overload less than
-    totalTests++;
-    logTestResult("Test " + std::to_string(totalTests) + " Operator<", helloStr < worldStr, logFile, successfulTests);
-
-    // Test operator overload addition
-    totalTests++;
-    String addedString = helloStr + " Kabiru";
-    logTestResult("Test " + std::to_string(totalTests) + " Operator+", addedString.EqualTo("Mr. Hello,  Bonface Kabiru"), logFile, successfulTests);
-
-    // Test operator overload addition assignment
-    totalTests++;
-    helloStr += " Kabiru";
-    logTestResult("Test " + std::to_string(totalTests) + " Operator+=", helloStr.EqualTo("Mr. Hello,  Bonface Kabiru"), logFile, successfulTests);
-
-    // Calculate percentage of successful tests
-    double successPercentage = (static_cast<double>(successfulTests) / totalTests) * 100;
-
-    // successful percentage
-    logFile << "Successful " << std::fixed << std::setprecision(2) << successPercentage << "%" << std::endl;
-
-    logFile.close();
-
-    return 0;
-}
+//#include "String.h"
+//#include <iostream>
+//#include <fstream>
+//#include <ctime>
+//#include <iomanip>
+//#include <string>
+//
+////log test results to a file
+//static void logTestResult(const std::string& testName, bool passed, std::ofstream& logFile, int& successfulTests) {
+//    logFile << testName << " " << (passed ? "Successful" : "Failed") << std::endl;
+//    if (passed) successfulTests++;
+//}
+//
+//int main() {
+//    std::ofstream logFile("log.txt", std::ios::app);
+//
+//    std::time_t t = std::time(nullptr);
+//    std::tm tm;
+//    localtime_s(&tm, &t);
+//    char buffer[26];
+//    asctime_s(buffer, sizeof buffer, &tm);
+//
+//    logFile << "\nDate: " << std::put_time(&tm, "%d/%m/%Y") << " Time: " << std::put_time(&tm, "%H:%M:%S") << std::endl;
+//
+//    String helloStr("Hello, ");
+//    String worldStr("World");
+//    String turkanaStr("Hello Turkana");
+//    String findAndReplaceString("This is a test string to be used for search and replace");
+//
+//    int totalTests = 0;
+//    int successfulTests = 0;
+//
+//    // Test concatenation
+//    totalTests++;
+//    String combineString = helloStr + worldStr;
+//    logTestResult("Test " + std::to_string(totalTests) + " Concatenation", combineString.EqualTo("Hello, World"), logFile, successfulTests);
+//
+//    // Test length of the combined string
+//    totalTests++;
+//    logTestResult("Test " + std::to_string(totalTests) + " Length", combineString.Length() == 12, logFile, successfulTests);
+//
+//    // Test character at index
+//    totalTests++;
+//    logTestResult("Test " + std::to_string(totalTests) + " CharacterAt", combineString.CharacterAt(0) == 'H', logFile, successfulTests);
+//
+//    // Test string equality
+//    totalTests++;
+//    logTestResult("Test " + std::to_string(totalTests) + " EqualTo", combineString.EqualTo("Hello, World"), logFile, successfulTests);
+//
+//    // Test upper and lower case
+//    totalTests++;
+//    logTestResult("Test " + std::to_string(totalTests) + " ToUpper", combineString.ToUpper().EqualTo("HELLO, WORLD"), logFile, successfulTests);
+//    totalTests++;
+//    logTestResult("Test " + std::to_string(totalTests) + " ToLower", combineString.ToLower().EqualTo("hello, world"), logFile, successfulTests);
+//
+//    // Test prepend
+//    totalTests++;
+//    logTestResult("Test " + std::to_string(totalTests) + " Prepend", helloStr.Prepend("Mr. ").EqualTo("Mr. Hello, "), logFile, successfulTests);
+//
+//    // Test append
+//    totalTests++;
+//    logTestResult("Test " + std::to_string(totalTests) + " Append", helloStr.Append(" Bonface").EqualTo("Mr. Hello,  Bonface"), logFile, successfulTests);
+//
+//    // Test find
+//    totalTests++;
+//    size_t foundIndex = findAndReplaceString.Find("search");
+//    logTestResult("Test " + std::to_string(totalTests) + " Find", foundIndex != SIZE_MAX, logFile, successfulTests);
+//
+//    // Test find from index
+//    totalTests++;
+//    size_t findIndex = findAndReplaceString.Find(10, "string");
+//    logTestResult("Test " + std::to_string(totalTests) + " FindFromIndex", findIndex != SIZE_MAX, logFile, successfulTests);
+//
+//    // Test replace
+//    totalTests++;
+//    turkanaStr.Replace("Hello", "Hi");
+//    logTestResult("Test " + std::to_string(totalTests) + " Replace", turkanaStr.EqualTo("Hi Turkana"), logFile, successfulTests);
+//
+//    // Test copy
+//    totalTests++;
+//    String copiedString = helloStr;
+//    logTestResult("Test " + std::to_string(totalTests) + " Copy", copiedString.EqualTo(helloStr), logFile, successfulTests);
+//
+//    // Test operator overload equality
+//    totalTests++;
+//    logTestResult("Test " + std::to_string(totalTests) + " Operator==", helloStr == helloStr, logFile, successfulTests);
+//
+//    // Test operator overload assignment
+//    totalTests++;
+//    String smallString("This is smaller");
+//    String largeString("This is a larger string to test assignment");
+//    smallString = largeString;
+//    logTestResult("Test " + std::to_string(totalTests) + " Operator=", smallString.Length() == largeString.Length(), logFile, successfulTests);
+//
+//    // Test operator overload subscript
+//    totalTests++;
+//    logTestResult("Test " + std::to_string(totalTests) + " Operator[]", helloStr[0] == 'M', logFile, successfulTests);
+//
+//    // Test operator overload inequality
+//    totalTests++;
+//    logTestResult("Test " + std::to_string(totalTests) + " Operator!=", helloStr != worldStr, logFile, successfulTests);
+//
+//    // Test operator overload less than
+//    totalTests++;
+//    logTestResult("Test " + std::to_string(totalTests) + " Operator<", helloStr < worldStr, logFile, successfulTests);
+//
+//    // Test operator overload addition
+//    totalTests++;
+//    String addedString = helloStr + " Kabiru";
+//    logTestResult("Test " + std::to_string(totalTests) + " Operator+", addedString.EqualTo("Mr. Hello,  Bonface Kabiru"), logFile, successfulTests);
+//
+//    // Test operator overload addition assignment
+//    totalTests++;
+//    helloStr += " Kabiru";
+//    logTestResult("Test " + std::to_string(totalTests) + " Operator+=", helloStr.EqualTo("Mr. Hello,  Bonface Kabiru"), logFile, successfulTests);
+//
+//    // Calculate percentage of successful tests
+//    double successPercentage = (static_cast<double>(successfulTests) / totalTests) * 100;
+//
+//    // successful percentage
+//    logFile << "Successful " << std::fixed << std::setprecision(2) << successPercentage << "%" << std::endl;
+//
+//    logFile.close();
+//
+//    return 0;
+//}
--- String.cpp
+++ String.cpp
@@ -1,289 +1,289 @@
-	#include <iostream>
-	#include "String.h"
-	#include <cstring>
-	#include <cstddef>
+#include <iostream>
+#include "String.h"
+#include <cstring>
+#include <cstddef>
 
-	//default constructor
-	String::String() : m_str(nullptr), m_length(0), m_capacity(0){}
+//default constructor
+String::String() : m_str(nullptr), m_length(0), m_capacity(0) {}
 
-	//constructor C-style
-	String::String(const char* _str)
+//constructor C-style
+String::String(const char* _str)
+{
+	m_length = strlen(_str);
+	m_capacity = m_length + 1;
+	m_str = new char[m_capacity];
+	strcpy_s(m_str, m_capacity, _str);
+}
+
+//copy constructor deep coopy
+String::String(const String& _other)
+{
+	m_length = _other.m_length;
+	m_capacity = _other.m_capacity;
+	m_str = new char[m_capacity];
+	strcpy_s(m_str, m_capacity, _other.CStr());
+}
+
+//destructor
+String::~String()
+{
+	delete[] m_str;
+}
+
+//length of the string
+size_t String::Length() const
+{
+	return m_length;
+}
+
+//character at specified index
+char& String::CharacterAt(size_t _index)
+{
+	return m_str[_index];
+}
+
+//character at specified index (const)
+const char& String::CharacterAt(size_t _index) const
+{
+	return m_str[_index];
+}
+
+//check if the string is equal to another
+bool String::EqualTo(const String& _other) const
+{
+	return strcmp(m_str, _other.CStr()) == 0;
+}
+
+//append
+String& String::Append(const String& _str)
+{
+	//calculate new length and capacity
+	size_t newLength = m_length + _str.m_length;
+	size_t newCapacity = newLength + 1;
+
+	char* temp = new char[newCapacity];
+
+	//copy and append characters from m_str
+	strcpy_s(temp, newCapacity, m_str);
+	strcat_s(temp, newCapacity, _str.CStr());
+
+	delete[] m_str;
+
+	//update member variables
+	m_str = temp;
+	m_length = newLength;
+	m_capacity = newCapacity;
+
+	return *this;
+}
+
+//prepend
+String& String::Prepend(const String& _str)
+{
+	//calculate new length and capacity
+	size_t newLength = m_length + _str.m_length;
+	size_t newCapacity = newLength + 1;
+
+	char* temp = new char[newCapacity];
+
+	//copy and append characters from m_str
+	strcpy_s(temp, newCapacity, _str.CStr());
+	strcat_s(temp, newCapacity, m_str);
+
+	delete[] m_str;
+
+	//update member variables
+	m_str = temp;
+	m_length = newLength;
+	m_capacity = newCapacity;
+
+	return *this;
+}
+
+//C-style string
+const char* String::CStr() const
+{
+	return m_str;
+}
+
+//lowercase
+String& String::ToLower()
+{
+	for (size_t i = 0; i < m_length; i++)
 	{
-		m_length = strlen(_str);
-		m_capacity = m_length + 1;
-		m_str = new char[m_capacity];
-		strcpy_s(m_str, m_capacity, _str);
+		if (m_str[i] >= 'A' && m_str[i] <= 'Z')
+		{
+			m_str[i] += 32; //convert to lowercase
+		}
 	}
+	return *this;
+}
 
-	//copy constructor deep coopy
-	String::String(const String& _other)
+//uppercase
+String& String::ToUpper()
+{
+	for (size_t i = 0; i < m_length; i++)
 	{
-		m_length = _other.m_length;
-		m_capacity = _other.m_capacity;
-		m_str = new char[m_capacity];
-		strcpy_s(m_str, m_capacity, _other.CStr());
+		if (m_str[i] >= 'a' && m_str[i] <= 'z')
+		{
+			m_str[i] -= 32;//convert to uppercase
+		}
 	}
+	return *this;
+}
 
-	//destructor
-	String::~String()
+//find
+size_t String::Find(const String& _str)
+{
+	return Find(0, _str);
+}
+
+//find from index
+size_t String::Find(size_t _startIndex, const String& _str)
+{
+	//calculate lenght and startIndex of the substring
+	size_t findLength = _str.m_length;
+	size_t index = _startIndex;
+
+	while (index < m_length - findLength + 1)
 	{
+		size_t i = 0;
+
+		//compare characters of the substring
+		while (i < findLength && m_str[index + i] == _str.m_str[i])
+		{
+			++i;
+		}
+		//check if matching
+		if (i == findLength)
+		{
+			return index;
+		}
+		++index;
+	}
+	return SIZE_MAX;
+}
+
+//replace
+String& String::Replace(const String& _find, const String& _replace)
+{
+	//get the lenght of find and replace
+	size_t findLength = _find.m_length;
+	size_t replaceLength = _replace.m_length;
+
+	//find the first occurance of the substring
+	size_t index = Find(_find);
+
+	while (index != SIZE_MAX)
+	{
+		//calculate the new length
+		size_t newLength = m_length - findLength + replaceLength;
+
+		//allocate memory for the new string
+		char* temp = new char[newLength + 1];
+
+		//copy characters before the found substring
+		strncpy_s(temp, newLength + 1, m_str, index);
+
+		//copy replacement string
+		strcat_s(temp, newLength + 1, _replace.CStr());
+
+		//copy characters after the found substring
+		strcat_s(temp, newLength + 1, m_str + index + findLength);
+
+		//delete the old string and update the length
 		delete[] m_str;
-	}
-
-	//length of the string
-	size_t String::Length() const
-	{
-		return m_length;
-	}
-
-	//character at specified index
-	char& String::CharacterAt(size_t _index)
-	{
-		return m_str[_index];
-	}
-
-	//character at specified index (const)
-	const char& String::CharacterAt(size_t _index) const
-	{
-		return m_str[_index];
-	}
-
-	//check if the string is equal to another
-	bool String::EqualTo(const String& _other) const 
-	{
-		return strcmp(m_str, _other.CStr()) == 0;
-	}
-
-	//append
-	String& String::Append(const String& _str)
-	{
-		//calculate new length and capacity
-		size_t newLength = m_length + _str.m_length;
-		size_t newCapacity = newLength + 1;
-
-		char* temp = new char[newCapacity];
-
-		//copy and append characters from m_str
-		strcpy_s(temp, newCapacity, m_str);
-		strcat_s(temp, newCapacity, _str.CStr());
-
-		delete[] m_str;
-
-		//update member variables
 		m_str = temp;
 		m_length = newLength;
-		m_capacity = newCapacity;
+		m_capacity = newLength + 1;
 
-		return *this;
+		//find the next occurrence of the substring
+		index = Find(index + replaceLength, _find);
 	}
+	return *this;
+}
 
-	//prepend
-	String& String::Prepend(const String& _str)
-	{
-		//calculate new length and capacity
-		size_t newLength = m_length + _str.m_length;
-		size_t newCapacity = newLength + 1;
+//read from console
+String& String::ReadFromConsole()
+{
+	char buffer[256];
+	std::cin.getline(buffer, 256);
 
-		char* temp = new char[newCapacity];
+	//assign input str to the object
+	*this = buffer;
+	return *this;
+}
 
-		//copy and append characters from m_str
-		strcpy_s(temp, newCapacity, _str.CStr());
-		strcat_s(temp, newCapacity, m_str);
+//write to console
+String& String::WriteToConsole()
+{
+	std::cout << m_str;
+	return *this;
+}
 
-		delete[] m_str;
+//operator overload equality
+bool String::operator==(const String& _other)
+{
+	return EqualTo(_other);
+}
 
-		//update member variables
-		m_str = temp;
-		m_length = newLength;
-		m_capacity = newCapacity;
+//operator overload inequality
+bool String::operator!=(const String& _other)
+{
+	return !EqualTo(_other);
+}
 
-		return *this;
-	}
-
-	//C-style string
-	const char* String::CStr() const
-	{
-		return m_str;
-	}
-
-	//lowercase
-	String& String::ToLower()
-	{
-		for (size_t i = 0; i < m_length; i++)
-		{
-			if (m_str[i] >= 'A' && m_str[i] <= 'Z')
-			{
-				m_str[i] += 32; //convert to lowercase
-			}
-		}
-		return *this;
-	}
-
-	//uppercase
-	String& String::ToUpper()
-	{
-		for (size_t i = 0; i < m_length; i++)
-		{
-			if (m_str[i] >= 'a' && m_str[i] <= 'z')
-			{
-				m_str[i] -= 32;//convert to uppercase
-			}
-		}
-		return *this;
-	}
-
-	//find
-	size_t String::Find(const String& _str)
-	{
-		return Find(0, _str);
-	}
-
-	//find from index
-	size_t String::Find(size_t _startIndex, const String& _str)
-	{
-		//calculate lenght and startIndex of the substring
-		size_t findLength = _str.m_length;
-		size_t index = _startIndex;
-
-		while (index < m_length - findLength + 1)
-		{
-			size_t i = 0;
-
-			//compare characters of the substring
-			while (i < findLength && m_str[index + i] == _str.m_str[i])
-			{
-				++i;
-			}
-			//check if matching
-			if (i == findLength)
-			{
-				return index;
-			}
-			++index;
-		}
-		return SIZE_MAX;
-	}
-
-	//replace
-	String& String::Replace(const String& _find, const String& _replace)
-	{
-		//get the lenght of find and replace
-		size_t findLength = _find.m_length;
-		size_t replaceLength = _replace.m_length;
-
-		//find the first occurance of the substring
-		size_t index = Find(_find);
-
-		while (index != SIZE_MAX)
-		{
-			//calculate the new length
-			size_t newLength = m_length - findLength + replaceLength;
-
-			//allocate memory for the new string
-			char* temp = new char[newLength + 1];
-
-			//copy characters before the found substring
-			strncpy_s(temp, newLength + 1, m_str, index);
-
-			//copy replacement string
-			strcat_s(temp, newLength + 1, _replace.CStr());
-
-			//copy characters after the found substring
-			strcat_s(temp, newLength + 1, m_str + index + findLength);
-
-			//delete the old string and update the length
-			delete[] m_str;
-			m_str = temp;
-			m_length = newLength;
-			m_capacity = newLength + 1;
-
-			//find the next occurrence of the substring
-			index = Find(index + replaceLength, _find);
-		}
-		return *this;
-	}
-
-	//read from console
-	String& String::ReadFromConsole()
-	{
-		char buffer[256];
-		std::cin.getline(buffer, 256);
-
-		//assign input str to the object
-		*this = buffer;
-		return *this;
-	}
-
-	//write to console
-	String& String::WriteToConsole()
-	{
-		std::cout << m_str;
-		return *this;
-	}
-
-	//operator overload equality
-	bool String::operator==(const String& _other)
-	{
-		return EqualTo(_other);
-	}
-
-	//operator overload inequality
-	bool String::operator!=(const String& _other)
-	{
-		return !EqualTo(_other);
-	}
-
-	//operator overload assignment
-	String& String::operator=(const String& _str)
+//operator overload assignment
+String& String::operator=(const String& _str)
+{
+	if (this != &_str)
 	{
 		if (this != &_str)
 		{
-			if (this != &_str)
-			{
-				//update capacity
-				if (m_capacity < _str.m_length + 1) {
-					delete[] m_str;
-					m_capacity = _str.m_length + 1;
-					m_str = new char[m_capacity];
-				}
-
-				//copy the string and update length
-				m_length = _str.m_length;
-				strcpy_s(m_str, m_capacity, _str.CStr());
+			//update capacity
+			if (m_capacity < _str.m_length + 1) {
+				delete[] m_str;
+				m_capacity = _str.m_length + 1;
+				m_str = new char[m_capacity];
 			}
-			return *this;
+
+			//copy the string and update length
+			m_length = _str.m_length;
+			strcpy_s(m_str, m_capacity, _str.CStr());
 		}
-	}
-
-	//operator overload subscript
-	char& String::operator[](size_t _index)
-	{
-		return CharacterAt(_index);
-	}
-
-	//operator overload subscript(const)
-	const char& String::operator[](size_t _index) const
-	{
-		return CharacterAt(_index);
-	}
-
-	//operator overload less than
-	bool String::operator<(const String& _other) const
-	{
-		return strcmp(m_str, _other.CStr()) < 0;
-	}
-
-	//operator overload addition
-	String String::operator+(const String& _other) const
-	{
-		String result = *this;
-		result.Append(_other);
-		return result;
-	}
-
-	//operator overload addition assignment
-	String& String::operator+=(const String& _other)
-	{
-		Append(_other);
 		return *this;
 	}
+}
+
+//operator overload subscript
+char& String::operator[](size_t _index)
+{
+	return CharacterAt(_index);
+}
+
+//operator overload subscript(const)
+const char& String::operator[](size_t _index) const
+{
+	return CharacterAt(_index);
+}
+
+//operator overload less than
+bool String::operator<(const String& _other) const
+{
+	return strcmp(m_str, _other.CStr()) < 0;
+}
+
+//operator overload addition
+String String::operator+(const String& _other) const
+{
+	String result = *this;
+	result.Append(_other);
+	return result;
+}
+
+//operator overload addition assignment
+String& String::operator+=(const String& _other)
+{
+	Append(_other);
+	return *this;
+}
